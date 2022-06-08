21c67922a5ab0dd8681d175996335fa8e092b6e7
Albin Baby <albinbaby150@gmail.com>
2022-06-08 12:37:49 +0200

    encoded message

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
+def encode(plain_text, plain_shift):
+    encode_message = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + plain_shift
+        new_letter = alphabet[new_position]
+        encode_message += new_letter
+    return encode_message
+
+
+print(encode(plain_text=text, plain_shift=shift))
